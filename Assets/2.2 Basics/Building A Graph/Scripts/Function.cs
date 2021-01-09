0f96fd8e5b52bc1acd4a619f28eccec5701cee17
XieZehuai <1544498268@qq.com>
2021-01-10 04:16:52 +0800

    2.2 Basic - Building A Graph - Finish
    添加2D和3D函数图像

---
+++ Assets/2.2 Basics/Building A Graph/Scripts/Function.cs
@@ -0,0 +1,210 @@
+/*
+ * Author: Huai
+ * Create: 2021/1/10 3:57:32
+ *
+ * Description:
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Basics.BuildingAGraph
+{
+    /*
+     * 所有函数的枚举，每个值都必须有对应的相同名称的函数，不然会抛出异常
+     */
+    public enum Func2DEnum
+    {
+        FuncSin,
+        FuncSin1,
+        FuncCos
+    }
+
+    public enum Func3DEnum
+    {
+        FuncMultiSin1,
+        FuncMultiSin2,
+        FuncMultiSin3,
+        Ripple,
+        Cylinder,
+        Sphere,
+        Custom
+    }
+
+    public delegate Vector3 GraphFunction(float u, float v);
+
+    public class Function
+    {
+        public static Function Instance { get; } = new Function();
+
+        private static Dictionary<Func3DEnum, GraphFunction> funcDic;
+        private const float pi = Mathf.PI;
+
+        private Function()
+        {
+            funcDic = new Dictionary<Func3DEnum, GraphFunction>();
+        }
+
+        /*
+         * 二维函数调用接口，生成二维图形
+         */
+        public float Func2D(float x, Func2DEnum func2D)
+        {
+            // 用反射动态根据函数枚举获取对应的函数并执行，获取的函数必须是public的
+            // 效率比较低
+            Type type = typeof(Function);
+            MethodInfo methodInfo = type.GetMethod(func2D.ToString());
+
+            if (methodInfo == null)
+            {
+                Debug.Log($"获取函数{func2D.ToString()}失败");
+                return 0;
+            }
+
+            return (float)methodInfo.Invoke(Instance, new object[] { x });
+        }
+
+
+
+        /*
+         * 三维函数调用接口，生成三维图形
+         */
+        public Vector3 Func3D(float u, float v, Func3DEnum func)
+        {
+            // 用委托的方法实现根据枚举执行对应函数，函数可以是静态的，还可以是私有的
+            if (!funcDic.ContainsKey(func))
+            {
+                try
+                {
+                    // 动态生成枚举对应的函数委托，并添加到字典中
+                    GraphFunction function =
+                        (GraphFunction)Delegate.CreateDelegate(typeof(GraphFunction), this, func.ToString());
+                    funcDic.Add(func, function);
+                }
+                catch (ArgumentException)
+                {
+                    Debug.Log("无法加载函数：" + func);
+                }
+            }
+
+            return funcDic[func](u, v);
+        }
+
+        /*
+         * 带有一个参数的函数
+         */
+        public float FuncSin(float x)
+        {
+            return 2f * Mathf.Sin(pi * (x + Time.time));
+        }
+
+        public float FuncSin1(float x)
+        {
+            float y = Mathf.Sin(pi * (x + Time.time));
+            y += Mathf.Sin(2f * pi * (x + 2f * Time.time)) / 2f;
+            y /= 1.5f;
+            return y;
+        }
+
+        public float FuncCos(float x)
+        {
+            return Mathf.Cos(pi * (x + Time.time));
+        }
+
+
+
+        /*
+         * 带有两个参数的函数
+         */
+        private Vector3 FuncMultiSin1(float u, float v)
+        {
+            Vector3 position = new Vector3 { x = u, y = Mathf.Sin(pi * (u + v + Time.time)), z = v };
+            return position;
+        }
+
+        private Vector3 FuncMultiSin2(float u, float v)
+        {
+            Vector3 position = new Vector3();
+            position.x = u;
+
+            float y = Mathf.Sin(pi * (u + Time.time));
+            y += Mathf.Sin(pi * (v + Time.time));
+            y *= 0.5f;
+            position.y = y;
+
+            position.z = v;
+
+            return position;
+        }
+
+        private Vector3 FuncMultiSin3(float u, float v)
+        {
+            Vector3 position = new Vector3();
+            position.x = u;
+
+            float y = 4f * Mathf.Sin(pi * (u + v + Time.time * 0.5f));
+            y += Mathf.Sin(pi * (u + Time.time));
+            y += Mathf.Sin(2f * pi * (v + Time.time * 2f)) * 0.5f;
+            y /= 5.5f;
+            position.y = y;
+
+            position.z = v;
+
+            return position;
+        }
+
+        private Vector3 Ripple(float u, float v)
+        {
+            Vector3 position = new Vector3();
+            position.x = u;
+
+            float d = Mathf.Sqrt(u * u + v * v);
+            float y = Mathf.Sin(pi * (4f * d - Time.time * 2f));
+            y /= 10f * d + 1f;
+            position.y = y;
+
+            position.z = v;
+            return position;
+        }
+
+        private Vector3 Cylinder(float u, float v)
+        {
+            float radius = 0.8f + Mathf.Sin(pi * (6f * u + 2f * v + Time.time)) * 0.2f;
+            Vector3 position = new Vector3();
+
+            position.x = radius * Mathf.Sin(pi * u);
+            position.y = v;
+            position.z = radius * Mathf.Cos(pi * u);
+            return position;
+        }
+
+        private Vector3 Sphere(float u, float v)
+        {
+            Vector3 position = new Vector3();
+            float radius = 0.8f + Mathf.Sin(pi * (6f * u + Time.time)) * 0.1f;
+            radius += Mathf.Sin(pi * (4f * v + Time.time)) * 0.1f;
+            float s = radius * Mathf.Cos(pi * v * 0.5f);
+
+            position.x = s * Mathf.Cos(pi * u);
+            position.y = radius * Mathf.Sin(pi * v * 0.5f);
+            position.z = s * Mathf.Sin(pi * u);
+
+            return position;
+        }
+
+        private Vector3 Custom(float u, float v)
+        {
+            Vector3 position = new Vector3();
+            float delta = 1f + Mathf.Sin(pi * (6f * u + 3f * v + Time.time));
+            float d = 1f + Mathf.Cos(pi * (v * Time.time));
+
+            position.x = (u + v) * delta;
+            position.y = (u - v);
+            position.z = u * v * d;
+
+            return position;
+        }
+    }
+}
--- Assets/2.2 Basics/Building A Graph/Scripts/Graph.cs
+++ Assets/2.2 Basics/Building A Graph/Scripts/Graph.cs
@@ -6,45 +6,99 @@
  */
 
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace Basics
+namespace Basics.BuildingAGraph
 {
     public class Graph : MonoBehaviour
     {
-        [SerializeField] private Transform pointPrefab = default;
-        [SerializeField, Range(10, 100)] private int resolution = 10; // 分辨率，也就是点的数量
+        private readonly List<Transform> points = new List<Transform>(); // 储存所有点的实体
+        private int length; // points列表的长度（用一维数组表示二维数组，长度为（pointNum * 2）^ 2）
 
-        private Transform[] points;
+        [SerializeField] private Transform pointPrefabs; // 点的预设
+        [SerializeField] private int pointNum = 10; // 点的数量（数量为（pointNum * 2）^ 2）
+        [SerializeField] [Range(0.01f, 1f)] private float scale = 0.2f; // 点的缩放
+        [SerializeField] [Range(0.01f, 1f)] private float interval = 0.1f; // 点之间的距离
+        [SerializeField] private Func2DEnum func2D = default; // 2D图像函数
+        [SerializeField] private Func3DEnum func3D = default; // 3D图像函数
+        [SerializeField] private bool show3DGraph = default; // 是否显示3D图像，通过它来控制显示2D还是3D图像
+
+        // 暂时不用，在OnValueChange里使用
+        // private float scaleTemp;
+        // private float intervalTemp;
 
         private void Awake()
         {
-            points = new Transform[resolution];
-            float step = 2f / resolution;
-            Vector3 position = Vector3.zero;
-            Vector3 scale = Vector3.one * step;
+            // 暂时不用，在OnValueChange里使用
+            // scaleTemp = scale;
+            // intervalTemp = interval;
+        }
 
-            for (int i = 0; i < points.Length; i++)
+        private void Start()
+        {
+            // 获取点列表长度并实例化点
+            length = (int)Mathf.Pow(pointNum * 2, 2);
+            for (int i = 0; i < length; i++)
             {
-                points[i] = Instantiate(pointPrefab, transform);
-
-                position.x = (i + 0.5f) * step - 1f;
-                points[i].localPosition = position;
-
-                points[i].localScale = scale;
+                points.Add(Instantiate(pointPrefabs, transform));
             }
+
+            // DrawGraph();
         }
 
         private void Update()
         {
-            float time = Time.time;
-            for (int i = 0; i < points.Length; i++)
+            // OnValueChange();
+            DrawGraph(); // 画图，在Update里调用让图像能根据数据的变化实时改变
+        }
+
+        // 画图
+        private void DrawGraph()
+        {
+            for (float v = 0; v < pointNum * 2; v++)
             {
-                Transform point = points[i];
-                Vector3 position = point.localPosition;
-                position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-                point.localPosition = position;
+                for (float u = 0; u < pointNum * 2; u++)
+                {
+                    float index = v * pointNum * 2 + u; // 把二维坐标转化为一维坐标
+                    float y; // 点的y坐标的值
+                             // 判断要画的是2D图像还是3D图像
+                    if (show3DGraph)
+                    {
+                        Vector3 position = Function.Instance.Func3D(u / pointNum - 1f, v / pointNum - 1f, func3D);
+                        DrawPoint(points[(int)index], position * interval * 20f);
+                    }
+                    else
+                    {
+                        y = Function.Instance.Func2D(u / pointNum - 1f, func2D);
+                        DrawPoint(points[(int)index], (u - pointNum) * interval, y * interval * 10f, (v - pointNum) * interval);
+                    }
+                }
             }
         }
+
+        private void DrawPoint(Transform point, float x, float y, float z = 0f)
+        {
+            point.position = new Vector3(x, y, z);
+            point.localScale = Vector3.one * scale;
+        }
+
+        private void DrawPoint(Transform point, Vector3 position)
+        {
+            point.position = position;
+            point.localScale = Vector3.one * scale;
+        }
+
+        // 暂时不用，如果不是在Update里调用DrawGraph方法的话，可以用这个方法动态更新图像
+        // private void OnValueChange()
+        // {
+        //     if (scale != scaleTemp || interval != intervalTemp)
+        //     {
+        //         // Debug.Log("change");
+        //         DrawGraph();
+        //         scaleTemp = scale;
+        //         intervalTemp = interval;
+        //     }
+        // }
     }
 }
