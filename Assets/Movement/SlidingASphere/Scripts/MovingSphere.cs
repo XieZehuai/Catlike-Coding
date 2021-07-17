8ff881680b721be46c12d5811b27e430c88c702e
XieZehuai <1544498268@qq.com>
2021-07-18 00:56:20 +0800

    movement - physics - 2.2 jump height

--- Assets/Movement/SlidingASphere/Scripts/MovingSphere.cs
+++ Assets/Movement/SlidingASphere/Scripts/MovingSphere.cs
@@ -9,12 +9,16 @@ namespace Movement
         [SerializeField, Range(0f, 100f)] private float speed = 10f;
         [Header("小球最大加速度")]
         [SerializeField, Range(0f, 100f)] private float maxAcceleration = 10f;
-        [Header("小球可移动区域")]
-        [SerializeField] private Rect allowedArea = new Rect(-5f, -5f, 10f, 10f);
-        [Header("小球碰到边界后反弹力量的大小")]
-        [SerializeField, Range(0f, 1f)] private float bounciness = 0.5f;
 
-        private Vector3 velocity;
+        private new Rigidbody rigidbody;
+        private Vector3 velocity; // 当前速度
+        private Vector3 desiredVelocity; // 根据移动输入计算得出的目标速度
+        private bool desiredJump; // 是否要跳跃（在Update里检测，FixedUpdate里执行）
+
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+        }
 
         private void Update()
         {
@@ -23,39 +27,31 @@ namespace Movement
             playerInput.x = Input.GetAxis("Horizontal");
             playerInput.y = Input.GetAxis("Vertical");
             playerInput = Vector2.ClampMagnitude(playerInput, 1f); // 将输入向量的值限制在1以内
+            desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * speed; // 计算目标速度
 
-            Vector3 desiredVelocity = new Vector3(playerInput.x, 0f, playerInput.y) * speed; // 计算目标速度
+            desiredJump |= Input.GetButtonDown("Jump");
+        }
+
+        private void FixedUpdate()
+        {
+            velocity = rigidbody.velocity;
             float maxSpeedChange = maxAcceleration * Time.deltaTime; // 计算加速度
             // 让当前速度增加到目标速度
             velocity.x = Mathf.MoveTowards(velocity.x, desiredVelocity.x, maxSpeedChange);
             velocity.z = Mathf.MoveTowards(velocity.z, desiredVelocity.z, maxSpeedChange);
-            Vector3 displacement = velocity * Time.deltaTime; // 计算位移
 
-            Vector3 newPosition = transform.localPosition + displacement; // 计算移动后的新位置
-
-            if (newPosition.x < allowedArea.xMin)
+            if (desiredJump)
             {
-                newPosition.x = allowedArea.xMin;
-                velocity.x = -velocity.x * bounciness;
-            }
-            else if (newPosition.x > allowedArea.xMax)
-            {
-                newPosition.x = allowedArea.xMax;
-                velocity.x = -velocity.x * bounciness;
+                desiredJump = false;
+                Jump();
             }
 
-            if (newPosition.z < allowedArea.yMin)
-            {
-                newPosition.z = allowedArea.yMin;
-                velocity.z = -velocity.z * bounciness;
-            }
-            else if (newPosition.z > allowedArea.yMax)
-            {
-                newPosition.z = allowedArea.yMax;
-                velocity.z = -velocity.z * bounciness;
-            }
+            rigidbody.velocity = velocity;
+        }
 
-            transform.localPosition = newPosition; // 应用移动
+        private void Jump()
+        {
+            velocity.y += 5f;
         }
     }
 }
